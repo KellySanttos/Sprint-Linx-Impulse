9c2ac5a6cc8903e7997231b2a9a99507c76320a7
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 17:26:23 -0300

    iniciei  validação dos campos usando regex

---
+++ src/components/Form/useForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Email inválido',
+  },
+  cpf: {
+    regex: /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+    message: 'CPF inválido',
+  },
+};
