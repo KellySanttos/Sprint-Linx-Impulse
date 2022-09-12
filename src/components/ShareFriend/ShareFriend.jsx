497b43d796b56a7eea211b5d8d2dc7ed19f79298
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 18:28:00 -0300

    criei uma section com 2 divs e inputs para receber as informações do usuário convidado

--- src/components/ShareFriend/ShareFriend.jsx
+++ src/components/ShareFriend/ShareFriend.jsx
@@ -13,4 +13,14 @@ const FriendForm = () => {
             agora!
           </p>
 
-          
+          <section className={S.section}>
+        <div className={S.divInput}>
+          <Label texto="Nome do amigo:" />
+          <Input type="text" />
+        </div>
+        
+        <div className={S.divInput}>
+          <Label texto="E-mail do amigo:" />
+          <Input type="email" />
+        </div>
+      </section>
