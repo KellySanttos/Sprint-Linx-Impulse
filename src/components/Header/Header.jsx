3d0aa230d0677d90d655fe7e312683d470abc7a4
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 19:08:24 -0300

    criei uma const Header onde retornei um menu e h2 com informações

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import Button from '../Button/Button.jsx';
 
+const Header = () => {
+    return (
+      <header className={S.header}>
+        <Menu />
+        <h2 className={S.h2}>uma seleção de produtos</h2>
+
+        
