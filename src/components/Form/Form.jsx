e1bfb2a78faf29b35c77a97c5497c33b3aee950b
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 16:10:20 -0300

    criei uma const para adc os formulários

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,3 +3,18 @@ import Form from './Form.module.css';
 import useForm from './useForm';
 import Button from '../Button/Button.jsx';
 
+
+const Form = (type) => {
+    const name = useForm();
+    const email = useForm('email');
+    const cpf = useForm('cpf');
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      if (email.validate() && cpf.validate()) {
+        console.log('enviar');
+        alert('Enviado');
+      } else {
+        console.log('não enviar');
+      }
+    }
