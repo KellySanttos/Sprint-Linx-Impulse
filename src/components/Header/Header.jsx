6caab40389ddbb17961f75698d1fb5b7c953fc38
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 19:11:09 -0300

    adc um h1 e utilizei o span para separação na frase

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,11 @@ import Button from '../Button/Button.jsx';
 
 const Header = () => {
     return (
+    
       <header className={S.header}>
         <Menu />
         <h2 className={S.h2}>uma seleção de produtos</h2>
 
-        
+        <h1 className={S.h1}>
+        especial para <span className={S.diff}>você</span>
+      </h1>
