f4517a07cd51d12762f815862a8a401a55eaa494
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 15:41:04 -0300

    criei a pasta para os cards e estilizei

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from '../Button/Button';
+import Rating from '../Rating/Rating';
+import S from './Card.module.css';
+
+const Card = ({ img, name, description, oldValue, newValue, times, or }) => {
+  return (
+    <div className={S.div}>
+      <div className={S.img}>
+        <Rating />
+        <img className={S.img} src={img} alt="produto" />
+      </div>
+      <div className={S.content}>
+        <h2 className={S.h2}>{name}</h2>
+        <p className={S.desc}>{description}</p>
+        <p className={S.oldValue}>{`R$ ${oldValue},00`}</p>
+        <h4 className={S.h4}>{`R$ ${newValue},00`}</h4>
+        <p className={S.p}>
+          ou {times + 'x'} de: {'R$ ' + or}
+        </p>
+        <Button className={S.button} content="Comprar" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
