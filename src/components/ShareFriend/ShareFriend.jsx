036616cf76780f10ace5e9881215b0a57981ef45
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-12 18:24:53 -0300

    form e paragrafo para iniciar a interação com o usuário

--- src/components/ShareFriend/ShareFriend.jsx
+++ src/components/ShareFriend/ShareFriend.jsx
@@ -5,3 +5,12 @@ const FriendForm = () => {
       e.preventDefault();
       alert('Amigo Convidado!');
     }
+
+    return (
+        <form className={S.form}>
+          <p className={S.p}>
+            Quer que seus amigos também ganhem a lista personalizada deles? Preencha
+            agora!
+          </p>
+
+          
