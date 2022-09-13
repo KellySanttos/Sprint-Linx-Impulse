367ce96363e3f386a2954f7d768ba4980d40daf4
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-13 00:17:48 -0300

    criei uma const Input para

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,2 +1,21 @@
 import React from 'react';
 
+const Input = ({ id, type, value, error, onChange, onBlur, placeholder }) => {
+    return (
+      <>
+        <input
+          className={S.input}
+          type={type}
+          id={id}
+          name={id}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          placeholder={placeholder}
+        />
+        {error && <p className={S.p}>{error}</p>}
+      </>
+    );
+  };
+
+  
