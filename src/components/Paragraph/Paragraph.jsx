9cbab8b2c6d3f0734a0b32493ba4478b79244533
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-13 01:30:28 -0300

    adc um novo paragrafo

--- src/components/Paragraph/Paragraph.jsx
+++ src/components/Paragraph/Paragraph.jsx
@@ -16,3 +16,17 @@ const Paragraph = () => {
         with desktop publishing software like Aldus PageMaker including versions
         of Lorem Ipsum.
       </p>
+
+      <p className={S.p}>
+        The purpose of lorem ipsum is to create a natural looking block of text
+        (sentence, paragraph, page, etc.) that doesn't distract from the layout.
+        A practice not without controversy, laying out pages with meaningless
+        filler text can be very useful when the focus is meant to be on design,
+        not content. The passage experienced a surge in popularity during the
+        1960s when Letraset used it on their dry-transfer sheets, and again
+        during the 90s as desktop publishers bundled the text with their
+        software.
+      </p>
+    </div>
+  );
+};
