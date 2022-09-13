1596d5c1d69705188b112aaf585b2c22ced25503
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-13 02:07:56 -0300

    fiz a importações dos components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import Header from './components/Header/Header.jsx'
-import ShareFriend from './components/ShareFriend/ShareFriend.jsx'
-import './App.css'
+import { useState } from 'react';
+import Header from './components/Header/Header.jsx';
+import Main from './components/Main/Main.jsx';
+import Products from './components/Products/Products.jsx';
+import ShareFriend from './components/ShareFriend/ShareFriend.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import './App.css';
 
 function App() {
   return (
