97e2f205c2ce37e4ee57928a3d9ec78501173c9a
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-13 01:42:29 -0300

    adicionei o arquivo Products onde consumi a api para respostas de protudos

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,50 @@
+const Products = () => {
+    const [card, setNewCard] = useState([]);
+    const [section, setSection] = useState(1);
+  
+    const handleReq = async () => {
+      const url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1${section}`;
+      const response = await fetch(url);
+      const json = await response.json();
+      setNewCard([...card, ...json.products]);
+    };
+  
+    function nextSection() {
+      setSection(section + 1);
+    }
+  
+    useEffect(() => {
+      handleReq();
+    }, [section]);
+  
+    return (
+      <div className={S.div}>
+        <Separator text="Sua seleção especial" />
+        <div className={S.page}>
+          {card.length > 0 &&
+            card.map((card, index) => {
+              return (
+                <Card
+                  img={card.image}
+                  name={card.name}
+                  description={card.description}
+                  oldValue={card.oldPrice}
+                  newValue={card.price}
+                  times={card.installments.count}
+                  or={card.installments.value}
+                  key={index}
+                />
+              );
+            })}
+        </div>
+        <Button
+          className={S.button}
+          onClick={nextSection}
+          content="Ainda mais produtos aqui"
+        />
+        <Separator text="Compartilhe a novidade" />
+      </div>
+    );
+  };
+  
+  export default Products;
