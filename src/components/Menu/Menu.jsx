ff1a4afb37bad8e014bc1d21c701fa42f71eb9fe
Kelly Santtos <93755351+KellySanttos@users.noreply.github.com>
2022-09-13 00:44:28 -0300

    criei o Menu.jsx onde adc botões, importei o css e exportei a const

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import S from './Menu.module.css';
+
+const Menu = () => {
+  return (
+    <div>
+      <nav className={S.nav}>
+        <button className={S.button}>Conheça a Linx</button>
+        <button className={S.button}>Ajude o algoritmo</button>
+        <button className={S.button}>Seus Produtos</button>
+        <button className={S.button}>Compartilhe</button>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
